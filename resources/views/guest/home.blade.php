6239094fdee15e3046026df10ba2bb6a27bd1291
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-04-28 09:20:26 +0200

    Style fix

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -7,18 +7,16 @@
         <div class="jumbo"></div>
         <div class="container">
             <h1>current series</h1>
-            <div class="grid">
-                <ul>
-                    @foreach ($comics as $comic)
-                        <li class="card">
-                            <div class="img-wrapper">
-                                <img src="{{ $comic["thumb"] }}" alt="">
-                            </div>
-                            <p>{{ $comic['title'] }}</p>
-                        </li>
-                    @endforeach
-                </ul>
-            </div>
+            <ul class="grid">
+                @foreach ($comics as $comic)
+                    <li class="card">
+                        <div class="img-wrapper">
+                            <img src="{{ $comic["thumb"] }}" alt="">
+                        </div>
+                        <p>{{ $comic['title'] }}</p>
+                    </li>
+                @endforeach
+            </ul>
             <div class="load-more_btn">
                 load more
             </div>
